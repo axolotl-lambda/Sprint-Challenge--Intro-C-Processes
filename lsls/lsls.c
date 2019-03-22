4aee919df30a5fcb5e10e0e404c111635e2b2592
Axolotl <samuelmachat@gmail.com>
2019-03-22 11:26:39 -0400

    command parsing and root directory reading done

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -7,11 +7,31 @@
 int main(int argc, char **argv)
 {
   // Parse command line
+  for (int i = 0; i < argc; i++)
+  {
+    // argv[i] is the argument at index i
+    printf("%s\n", argv[i]);
+  }
 
   // Open directory
+  DIR *dir;
+  struct dirent *entry;
 
+  dir = opendir("/");
+
+  if (dir == NULL)
+  {
+    perror("opendir() error");
+  }
   // Repeatly read and print entries
 
+  else
+  {
+    puts("contents of root:");
+    while ((entry = readdir(dir)) != NULL)
+      printf("  %s\n", entry->d_name);
+    closedir(dir);
+  }
   // Close directory
 
   return 0;
